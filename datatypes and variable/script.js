ef8da29de891b041d5e639d95e860f8fb69e1dae
yamaitech <eishworacharya@gmail.com>
2024-07-11 13:35:38 +0545

    datatypes and variable

---
+++ datatypes and variable/script.js
@@ -0,0 +1,58 @@
+console.log("hello world")
+//data types variables
+var names="ishwor acharya"
+console.log(names);
+//data types (2 type of  datatypes) => 1. primitive & 2. 
+// 1) primitive datatypes
+// String, Number, BigInt, Boolean, Null, Undefined, and Symbol
+/* 
+// integer
+let a=1;
+console.log(typeof(a))
+*/
+
+/*
+//String
+let a="ishwor";
+console.log(typeof(a));
+
+*/
+
+/*
+//boolean
+let a=true;
+console.log(typeof(a))
+*/
+
+//Undefined
+/*
+let a;
+console.log(typeof(a))
+*/
+
+//null  (In JavaScript null is "nothing",but the datatype of null is object)
+/*
+let a=null;
+console.log(typeof(a))
+*/
+
+//symbol (it means unique , though the datatypes and the values are the same , if used symbol it differs from eachother)
+/*
+let a,b;
+a="ishwor";
+b=Symbol("ishwor")
+console.log(a==b)
+*/
+
+//bigInt ( prints more digits than integer=>16 digits)
+/*
+let a=BigInt(1);
+console.log(a);
+*/
+
+
+
+
+
+
+//2. Non-primitive datatypes =>Objects, Arrays and Functions
