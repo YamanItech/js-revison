91a352f1c2c4c9c58843a58be5ce9bb4289c3cc1
yamaitech <eishworacharya@gmail.com>
2024-07-28 13:37:02 +0545

    readme edit

---
+++ operators/script.js
@@ -0,0 +1,22 @@
+//Operators
+/*
+The Assignment Operator = assigns values
+
+The Addition Operator + adds values
+
+The Multiplication Operator * multiplies values
+
+The Comparison Operator > compares values
+*/
+
+
+// 1. Arithemetic Operators
+let a,b;
+a=2;
+b=3;
+console.log(a+b);//5
+console.log(a-b);//-1
+console.log(a*b);//6
+console.log(a/b);//0.6666666666666666
+console.log(a%b);//2
+console.log(a++);//2
